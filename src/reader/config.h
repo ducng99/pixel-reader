c2522579ee902d7dec6db120522d702a70650b43
ducng99 <tom@tomng.dev>
2024-10-14 19:17:29 +1300

    fix: Fixed paths for rocknix system

--- src/reader/config.h
+++ src/reader/config.h
@@ -5,9 +5,13 @@
 
 #define IDLE_SAVE_TIME_SEC 60
 
-#define FONT_DIR            "resources/fonts"
-#define DEFAULT_FONT_NAME   "resources/fonts/DejaVuSans.ttf"
-#define SYSTEM_FONT         "resources/fonts/DejaVuSansMono.ttf"
+#ifndef DEFAULT_STORAGE
+#define DEFAULT_STORAGE "/storage/.config/pixelreader/"
+#endif
+
+#define FONT_DIR            DEFAULT_STORAGE "resources/fonts"
+#define DEFAULT_FONT_NAME   DEFAULT_STORAGE "resources/fonts/DejaVuSans.ttf"
+#define SYSTEM_FONT         DEFAULT_STORAGE "resources/fonts/DejaVuSansMono.ttf"
 
 #define MIN_FONT_SIZE      18
 #define MAX_FONT_SIZE      40
@@ -20,15 +24,10 @@
 #define DEFAULT_COLOR_THEME "night_contrast"
 
 #define CONFIG_FILE_PATH "reader.cfg"
-#define FALLBACK_STORE_PATH ".pixel_reader_store"
+#define FALLBACK_STORE_PATH DEFAULT_STORAGE ".pixel_reader_store"
 
-#if PLATFORM_MIYOO_MINI
-    #define DEFAULT_BROWSE_PATH "/mnt/SDCARD/Media/Books/"
-    #define EXTRA_FONTS_LIST    {"/customer/app/wqy-microhei.ttc"}
-#else
-    #define DEFAULT_BROWSE_PATH std::filesystem::current_path() / ""
-    #define EXTRA_FONTS_LIST    {}
-#endif
+#define DEFAULT_BROWSE_PATH "/roms/media/books/"
+#define EXTRA_FONTS_LIST {}
 
 #define DEFAULT_SHOW_PROGRESS true
 #define DEFAULT_SHOULDER_KEYMAP "LR"
